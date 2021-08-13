2ea48f2e82a30058a55b7b883da69b3a5284b3e3
myairin <irenewindagultom@gmail.com>
2021-08-13 20:15:07 +0700

    revisi tugas class kalkulator(eror)

--- Kalkulator.js
+++ Kalkulator.js
@@ -1,1 +1,16 @@
 // Tulis kode solusi di bawah ini
+
+class kalkulator {
+    constructor(tambah, kurang, kali, pangkat, akarPangkat, luasLingkaran) {
+        this.tambah = tambah;
+        this.kurang = kurang;
+        this.kali = kali; 
+        this.pangkat = pangkat;
+        this.akarPangkat = akarPangkat;
+        this.luasLingkaran = luasLingkaran;
+    }
+}
+
+var kalkulator1 = new kalkulator("Fungsi Penjumlahan(+)", "Fungsi Pengurangan(-)", "Fungsi Perkalian(x)",
+"Fungsi Pembagian(:)", " Fungsi Pangkat 2, 3 dan seterusnya(2²)", "Fungsi Akar Pangkat(√2²)", "Konstanta Pi, digunakan untuk meghitung lingkaran(π)")
+console.log(kalkulator1);
