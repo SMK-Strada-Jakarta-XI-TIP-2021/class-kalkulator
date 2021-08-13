7ebb391379a76bcc356d7e7f6fe4e518dcab9c8a
maxdesanta <maxdesanta1@gmail.com>
2021-08-13 22:12:00 +0700

    tugas js max perubahan pertama

--- Kalkulator.js
+++ Kalkulator.js
@@ -2,7 +2,7 @@
 // membuat class kalkulator
 class Kalkulator {
     // membuat constructor
-    constructor(angkaPenjumlahan1,angkaPenjumlahan2,angkaPengurangan1,angkaPengurangan2,angkaPerkalian1,angkaPerkalian2,angkaPembagian1,angkaPembagian2,angkaPangkat,akarPangkat,pi) {
+    constructor(angkaPenjumlahan1,angkaPenjumlahan2,angkaPengurangan1,angkaPengurangan2,angkaPerkalian1,angkaPerkalian2,angkaPembagian1,angkaPembagian2,angkaPangkat2,akarPangkatKuadrat,akarPangkatKibik,pi) {
         this.angkaPenjumlahan1 = angkaPenjumlahan1;
         this.angkaPenjumlahan2 = angkaPenjumlahan2;
         this.angkaPengurangan1 = angkaPengurangan1;
@@ -11,8 +11,9 @@ class Kalkulator {
         this.angkaPerkalian2 = angkaPerkalian2;
         this.angkaPembagian1 = angkaPembagian1;
         this.angkaPembagian2 = angkaPembagian2;
-        this.angkaPangkat = angkaPangkat;
-        this.akarPangkat = akarPangkat;
+        this.angkaPangkat2 = angkaPangkat2;
+        this.akarPangkatKuadrat = akarPangkatKuadrat;
+        this.akarPangkatKibik = akarPangkatKibik;
         this.pi = pi;
     }
     // operasi penjumlahan
@@ -33,7 +34,31 @@ class Kalkulator {
     }
     // operasi pangkat 2
     pangkatKuadrat() {
-        return this.angkaPangkat * this.angkaPangkat;
+        return this.angkaPangkat2 * this.angkaPangkat2;
+    }
+    // operasi akar pangkat 2
+    apangkatkuadrat() {
+        let number = this.akarPangkatKuadrat
+        for (let i = number; i >= 1; i--) {
+            if (i * i === number) {
+                number = i;
+                break;
+            }
+        }
+        return number;
+    }
+    // operasi akar pangkat 3
+    aPangkatkibik() {
+        let angka = this.akarPangkatKibik
+        let x = angka;
+        while (x >= 1) {
+            if (x * x * x === angka) {
+                angka = x;
+                break;
+            }
+            x--;
+        }
+        return angka;
     }
     // operasi luas 
     lLingkaran(x) {
@@ -47,11 +72,12 @@ class Kalkulator {
     }
 }
 
-const hasil = new Kalkulator(2, 3, 4, 5, 6, 6,6 ,3 ,7 ,8 , 3.14);
-console.log(hasil.penjumlahan());
-console.log(hasil.pengurangan());
-console.log(hasil.perkalian());
-console.log(hasil.pembagian());
-console.log(hasil.pangkatKuadrat());
-console.log(hasil.lLingkaran(7));
-
+const hasil = new Kalkulator(2, 3, 4, 5, 6, 6,6 ,3 ,7 ,16 , 9, 3.14);
+console.log(`Hasil Penjumlahan : ${hasil.penjumlahan()}`);
+console.log(`Hasil pengurangan : ${hasil.pengurangan()}`);
+console.log(`Hasil perkalian : ${hasil.perkalian()}`);
+console.log(`Hasil Pembagian : ${hasil.pembagian()}`);
+console.log(`Hasil pangkat kuadrat : ${hasil.pangkatKuadrat()}`);
+console.log(`Hasil akar kuadrat : ${hasil.apangkatkuadrat()}`);
+console.log(`Hasil akar kibik : ${hasil.aPangkatkibik()}`);
+console.log(`Hasil luas lingkaran : ${hasil.lLingkaran(7)}`);
