c4d2ae076c9e7eb8dd17d77dd000fc985b16c5c4
sulthanarif <msarizky201@gmail.com>
2021-08-16 00:59:25 +0700

    perubahan 1

--- Kalkulator.js
+++ Kalkulator.js
@@ -1,1 +1,9 @@
 // Tulis kode solusi di bawah ini
+
+class kalkulator {
+    // constructor
+    constructor(penjumlahan,pengurangan,perkalian,pembagian,pangkatKuadrat,pangkatKubik,pi) {
+        this.penjumlahan = penjumlahan
+    }
+    
+}
